bf12af0afcd7e041db7ea836f916c7ef23889f88
terrrra <thabangstephenmakume@gmail.com>
2023-07-18 22:36:07 +0200

    Committing the Updated Changes.

--- PayGateDotNetServer/Program.cs
+++ PayGateDotNetServer/Program.cs
@@ -11,6 +11,8 @@ namespace PayGateDotNetServer
 {
     public class Program
     {
+
+        
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
