15c1661441792d340848847d6b8fe6a88c21dfe8
terrrra <thabangstephenmakume@gmail.com>
2023-07-18 22:37:05 +0200

    Update Startup.

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -27,9 +28,11 @@ namespace PayGateDotNetServer
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddJsonOptions(x =>
+                x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
             services.AddSwaggerGen();
-            services.AddDbContext<PayGateDotNetServerDBContext>(options => options.UseInMemoryDatabase("CustomerDb"));
+            services.AddDbContext<PayGateDotNetServerDBContext>(options => options.UseSqlServer(Configuration.GetConnectionString("PayGateDotNetServerConnectionString")));
+            //services.AddDbContext<PayGateDotNetServerDBContext>(options => options.UseInMemoryDatabase("CustomerDb"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
