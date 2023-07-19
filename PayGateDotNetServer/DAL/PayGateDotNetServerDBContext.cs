97dfb1b333d6062a3518bec28438af378154cd47
terrrra <thabangstephenmakume@gmail.com>
2023-07-19 21:24:30 +0200

    Adding DBContext

--- PayGateDotNetServer/DAL/PayGateDotNetServerDBContext.cs
+++ PayGateDotNetServer/DAL/PayGateDotNetServerDBContext.cs
@@ -8,15 +8,13 @@ namespace PayGateDotNetServer.DAL
     {
         public PayGateDotNetServerDBContext(DbContextOptions options) : base(options) 
         { 
-                
         }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<CustomerAccount>().HasKey(vf => new { vf.CustomerId, vf.AccountId });
-
+            modelBuilder.Entity<Transaction>().HasOne(tv => tv.Customer).WithMany().HasForeignKey(tv => tv.CustomerId);
+            modelBuilder.Entity<Transaction>().HasOne(v => v.Account).WithMany().HasForeignKey(v => v.AccountId);
         }
-
         public DbSet<Customer> Customers { get; set; }
         public DbSet<CustomerAccount> CustomerAccounts { get; set; }
         public DbSet<Transaction> Transactions { get; set; }
