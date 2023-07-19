77a90e7489fa2828819bb0179f8ec52c1daf21bc
terrrra <thabangstephenmakume@gmail.com>
2023-07-19 21:23:58 +0200

    Migration added.

--- PayGateDotNetServer/Migrations/20230719184525_IntialMigration.Designer.cs
+++ PayGateDotNetServer/Migrations/20230719184525_IntialMigration.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PayGateDotNetServer.DAL;
 
 namespace PayGateDotNetServer.Migrations
 {
     [DbContext(typeof(PayGateDotNetServerDBContext))]
-    partial class PayGateDotNetServerDBContextModelSnapshot : ModelSnapshot
+    [Migration("20230719184525_IntialMigration")]
+    partial class IntialMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
